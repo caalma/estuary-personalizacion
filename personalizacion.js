5f75a2123a413dad2f03df25328916f0b30a2837
caalma <nds.suscript@gmail.com>
2024-03-21 23:13:33 -0300

    Ajuste

--- personalizacion.js
+++ personalizacion.js
@@ -1,11 +1,14 @@
-const css = [
-    "@import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&display=swap');",
-    "* { font-family: 'Fira Code' !important; letter-spacing: -0.05em; }"
-];
-var head = document.head || document.getElementsByTagName('head')[0],
-    style = document.createElement('style');
 
-head.appendChild(style);
+;(function(){
+    const css = [
+        "@import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&display=swap');",
+        "* { font-family: 'Fira Code' !important; letter-spacing: -0.05em; }"
+    ];
+    var head = document.head || document.getElementsByTagName('head')[0],
+        style = document.createElement('style');
 
-style.type = 'text/css';
-style.appendChild(document.createTextNode(css.join('\n')));
+    head.appendChild(style);
+
+    style.type = 'text/css';
+    style.appendChild(document.createTextNode(css.join('\n')));
+})()
